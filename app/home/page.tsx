580bcd9ee86edc08e7476f188ed5ce645fbe5b1a
JeanJano <jeansauvage6@gmail.com>
2024-08-21 17:32:55 +0200

    naming file

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import SearchInput from "@/components/searchInput";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Hello, world!</h1>
+            <p>
+                This is a simple example of a Next.js app with a custom layout.
+            </p>
+            <SearchInput />
+        </>
+    );
+}
---
+++ components/searchInput.tsx
@@ -0,0 +1,5 @@
+export default function SearchInput() {
+    return (
+        <input type="search" placeholder="Search..." />
+    );
+}
